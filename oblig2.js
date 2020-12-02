8eaea78fdaa2e4ba21cb8a0ee60cb20e6efb8df9
Eriksens93 <72790306+Eriksens93@users.noreply.github.com>
2020-12-02 11:04:57 +0100

    Add files via upload

--- oblig2.js
+++ oblig2.js
@@ -1,6 +1,7 @@
      // controller 
 
-     // For å velge stolpe
+    //  For å velge stolpe ved å klikke på den, skal også komme svart ramme(border) rundt. 
+    //  Hvis den ikke er selecta så selecter den som vil si at border settes til black (stroke)
 
      function selectedBar(barNo) {
          if (barNo == chosenBar) {
@@ -11,35 +12,43 @@
          show();
      }
 
-    //  Slette en stolpe. Splice funksjonen er for å fjerne en ting ut fra en array
-
-        //  Chosenbar starter på +1 i arrayet derfor bruker vi -1 her
-
+    //  Slette en stolpe. Splice funksjonen er for å fjerne et number ut fra et array
+    //  Chosenbar starter på +1 i arrayet derfor bruker vi -1 her
+    //  Hvor mange plasser i arrayet det skal fjerne er etter (-1). Hadde det stått 2 der så hadde den fjernet 2 stolper.
 
      function removeBar() {
         numbers.splice(chosenBar -1,  1);
         show();
      }
      
-    //  Endre en stolpe 
+    //  Endre en stolpe til ønsket verdi ved bruk av inputValue
 
      function changeBar() {
-         if (inputValue > 10 && inputValue <=0) {
+         if (inputValue > 10 || inputValue <=0) {
              alert('Feil, tallet er ikke mellom 1 og 10.');
          } else if(inputValue <= 10) {
              numbers [chosenBar -1 ] = parseInt(inputValue);
          }
+
+    // Avslutter inputvaluen her slik at den ikke blir "lagret" når man legger til ny stolpe 2 ganger
+
+         inputValue = '';
          show();
      }
 
-    //  Legge til en stolpe
-    // Number pusher verdien i arrayet som er innen inputvalue
+    // Legge til en stolpe ved bruk av inputValue
+    // Number pusher verdien (legger til ny bar) i arrayet som er i inputvalue
 
      function addBar() {
-         if (inputValue > 10) {
+         if (inputValue > 10 || inputValue <=0) {
              alert('Feil, tallet er ikke mellom 1 og 10');
         } else if (inputValue <= 10 ) {
             numbers.push(inputValue);
         }
+
+    // Avslutter inputvaluen her slik at den ikke blir "lagret" når man legger til ny stolpe 2 ganger
+
+            inputValue = '';
+        
         show();
      }
